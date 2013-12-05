4ad57602c595fa582a91e03f30b0c3b324028154
Dawenster <dstwen@gmail.com>
2013-12-04 16:43:47 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FsYvrScraper::Application.config.secret_key_base = '80c4656f5e39b32a2862930100fc84ac095bf0537bd0a7cb7626e85213eebd59fe4767479255e5a82b579f167493923a06022827b510e1048acecfe971d0c2e9'
